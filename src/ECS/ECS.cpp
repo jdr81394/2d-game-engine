ebd022673298fc67bba1e293d72054fd84fe8221
Jake Roberts <jake_roberts@colpal.com>
2022-02-06 04:23:56 -0500

    completed error checking and validation lecture

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -108,6 +108,11 @@ void Registry::GroupEntity(Entity entity, const std::string& group) {
 } 
 
 bool Registry::EntityBelongsToGroup(Entity entity, const std::string& group) const {
+    
+    if(entitiesPerGroup.find(group) == entitiesPerGroup.end()){
+        return false;
+    };
+
     auto groupEntities = entitiesPerGroup.at(group);
     return groupEntities.find(entity.GetId()) != groupEntities.end();
 }
